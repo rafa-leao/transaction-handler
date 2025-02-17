8ada16f0b98b4112d2bff901cf066ac0b4809870
Rafael Leão <rleao53@gmail.com>
2025-02-17 13:53:04 -0300

    first commit

---
+++ src/test/java/com/rafa/autorizador/AutorizadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafa.autorizador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutorizadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
