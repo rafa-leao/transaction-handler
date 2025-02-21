d5ed27f4bdddbfaf6d3a35cc5a5c468c1bf45c20
Rafael Leão <rleao53@gmail.com>
2025-02-20 21:50:45 -0300

    Cria validação dos campos Cartao

--- src/main/java/com/rafa/autorizador/cartao/Cartao.java
+++ src/main/java/com/rafa/autorizador/cartao/Cartao.java
@@ -1,5 +1,8 @@
 package com.rafa.autorizador.cartao;
 
+import java.math.BigDecimal;
+import java.util.Optional;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -15,20 +18,32 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Cartao {    
+public class Cartao {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(unique = true)
     private String numeroCartao;
     private String senha;
-    private Double saldo;
+    private BigDecimal saldo;
 
     public static Cartao converte(CartaoRecord cartaoRecord) {
         Cartao cartao = new Cartao();
         cartao.setNumeroCartao(cartaoRecord.numeroCartao());
         cartao.setSenha(cartaoRecord.senha());
-        cartao.setSaldo(500.0);
+        cartao.setSaldo(BigDecimal.valueOf(500.00));
         return cartao;
+    }
+
+    public Optional<Cartao> valida(String senha) {
+        return Optional.of(this).filter(c -> c.getSenha().equals(senha));
+    }
+
+    public Optional<Cartao> valida(BigDecimal valor) {
+        return Optional.of(this).filter(c -> c.getSaldo().compareTo(valor) >= 0);
+    }
+
+    public void subtraiSaldo(BigDecimal valor) {
+        this.saldo = this.saldo.subtract(valor);
     }
 }
